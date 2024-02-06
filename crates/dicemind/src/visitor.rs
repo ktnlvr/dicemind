283c30fc4c9b6b132a653728c15ba8fe74b4df0a
Kittenlover229 <artur.roos.artur@gmail.com>
2024-02-06 17:33:10 +0200

    Add visiting annotated expressions

--- crates/dicemind/src/visitor.rs
+++ crates/dicemind/src/visitor.rs
@@ -1,6 +1,6 @@
 use smallvec::SmallVec;
 
-use crate::syntax::{Augmentation, BinaryOperator, Expression, Integer};
+use crate::syntax::{AnnotationString, Augmentation, BinaryOperator, Expression, Integer};
 
 pub trait Visitor<T> {
     fn visit(&mut self, expr: Expression) -> T {
@@ -27,8 +27,8 @@ pub trait Visitor<T> {
             Subexpression(box e) => self.visit_subexpression(e),
             Annotated {
                 expression: box expr,
-                ..
-            } => self.visit(expr),
+                annotation,
+            } => self.visit_annotated(expr, annotation),
             UnaryNegation(box UnaryNegation(box v)) => self.visit(v),
             UnaryNegation(box v) => {
                 let v = self.visit(v);
@@ -49,6 +49,10 @@ pub trait Visitor<T> {
     fn visit_constant(&mut self, c: Integer) -> T;
 
     fn visit_binop(&mut self, op: BinaryOperator, lhs: T, rhs: T) -> T;
+
+    fn visit_annotated(&mut self, expr: Expression, _annotation: AnnotationString) -> T {
+        self.visit(expr)
+    }
 
     fn visit_subexpression(&mut self, subexpr: Expression) -> T {
         self.visit(subexpr)
